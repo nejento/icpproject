90ceebfd6b51482d89a7a6295250d0ff9f81bc3e
nejento <1355003+nejento@users.noreply.github.com>
2023-06-08 15:09:50 +0200

    Removed RNG

--- ICPWorking/ICPWorking.cpp
+++ ICPWorking/ICPWorking.cpp
@@ -524,7 +524,7 @@ void cursor_position_callback(GLFWwindow* window, double xpos, double ypos)
 //=== Main === 
 int main()
 {
-	std::cout << "Type \"r\" and comfirm with ENTER for realtime raster processing or anything else for the OpenGL application.\n";
+	std::cout << "Type \"r\" and confirm with ENTER for realtime raster processing or type anything else for the OpenGL application.\n";
 	char c;
 	std::cin.get(c);
 	
@@ -532,15 +532,6 @@ int main()
 		run_2D_raster_processing();
 		return 0 ;
 	}
-	// === Random number generator ===
-	std::mt19937_64 rng;
-	// initialize the random number generator with time-dependent seed
-	uint64_t timeSeed = std::chrono::high_resolution_clock::now().time_since_epoch().count();
-	std::seed_seq ss{ uint32_t(timeSeed & 0xffffffff), uint32_t(timeSeed >> 32) };
-	rng.seed(ss);
-	// initialize a uniform distribution between 0 and 1
-	std::uniform_real_distribution<double> unif(0, 1);
-
 
 	init_glfw();
 	init_glew();
